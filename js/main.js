e138409b8639c8fcc67430d9af3ddebe5d15f306
2952902226aA <132554136+2952902226aA@users.noreply.github.com>
2023-05-05 17:47:48 +0800

    beautify font

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
-// 待添加
+// js
+window.onload=function(){
+    var obt=document.getElementById("bu");
+    var odiv=document.getElementById("na");
+  obt.onclick=function(){
+    if(odiv.style.display=="none"){
+    odiv.style.display="block";
+    }
+  else{
+    odiv.style.display="none";
+    }
+      }}
